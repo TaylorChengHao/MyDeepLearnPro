c9dbf21a50e8097eabb8ca0d5e623cf5846c39af
TaylorChengHao <1335072703@qq.com>
2017-08-04 01:04:54 +0800

    edit Network __init__.py add sigmoid & feedforward func

--- Network/__init__.py
+++ Network/__init__.py
@@ -12,3 +12,11 @@ class Network(object):
         self.biases=[np.random.randn(y,1) for y in sizes[1:]]
         # 权重
         self.weights=[np.random.randn(y,x) for x,y in zip(sizes[:-1],sizes[1:])]
+
+    def sigmoid(self,z):
+        return  1.0/(1.0+np.exp(-z))
+
+    def feedforward(self,a):
+        for b,w in zip(self.biases,self.weights):
+            a=self.sigmoid(np.dot(w,a)+b)
+        return a
