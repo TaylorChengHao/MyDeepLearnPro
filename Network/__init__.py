f68f482fb2b282739e4a1569de3549943d362149
TaylorChengHao <1335072703@qq.com>
2017-08-04 10:48:26 +0800

    add SGD func

--- Network/__init__.py
+++ Network/__init__.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 import numpy as np
+import random
 
 class Network(object):
 
@@ -20,3 +21,17 @@ class Network(object):
         for b,w in zip(self.biases,self.weights):
             a=self.sigmoid(np.dot(w,a)+b)
         return a
+
+    def SGD(self,training_data,epochs,mini_batch_size,eta,test_data=None):
+        if test_data:n_test=len(test_data)
+        n=len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches=[training_data[k:k+mini_batch_size]
+                          for k in xrange(0,n,mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch,eta)
+            if test_data:
+                print "Epoch {0}:{1}/{2}".format(j,self.evaluate(test_data),n_test)
+            else:
+                print "Epoch {0} complete".format(j)
